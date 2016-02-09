1ec36d98fa1de6679ea577abecb899193db96c42
Anders <handiiandii@gmail.com>
2016-02-09 15:06:50 +0100

    File for creating geojson file for lat-long

---
+++ tools/geo_data.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3.5
+from collections import defaultdict
+import json
+import pprint
+
+def save_json_data(data, filename="geodata.geojson"):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+def generate_geojson(input_dict):
+    geo_dict = defaultdict(dict)
+
+    features = []
+    
+    
+    for count, t in enumerate(wanted_data.items()): #(user, timestamps)
+        feature = defaultdict(dict)
+        properties = defaultdict(dict)
+        geometry = defaultdict(dict)
+        geometry['type'] = "MultiLineString"
+
+        user, lat_long = t
+        feature['type'] = "feature"
+        feature['id'] = user
+        geometry['coordinates'] = [lat_long]
+        feature['geometry'] = geometry
+        features.append(feature)
+        if count >=3:
+            break
+        
+    geo_dict['type'] = "FeatureCollection"
+    geo_dict['features'] = features
+    return geo_dict
+
+
+
+raw_data = defaultdict(dict)
+wanted_data = defaultdict(dict)
+
+
+f = "all_201509.json"
+with open(f) as json_file:
+    raw_data = json.load(json_file)
+
+
+for data in raw_data:
+    if data['country'] == "Japan":
+        if data['useruuid'] in wanted_data:
+            wanted_data[data['useruuid']].append([data['latitude'], data['longitude']])
+        else:
+            wanted_data[data['useruuid']] = [[data['latitude'], data['longitude']]]
+
+
+
+geo_json = generate_geojson(wanted_data)
+save_json_data(geo_json)
+#pprint.pprint(geo_json)
+# for count, t in enumerate(wanted_data.items()): #(user, timestamps)
+#     user, timestamps = t
+#     if count >=7:
+#         break
+#     print("{0} = {1}".format(user, timestamps))
+
+

63044336983ddb355ee831ff171622e618647820
Søren Howe Gersager <syrelyre@gmail.com>
2016-02-15 16:30:09 +0100

    moved database helper to presentation_app (temporary)

--- presentation_app/app.py
+++ presentation_app/app.py
@@ -3,6 +3,7 @@ import flask
 import json
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort
+import database_helper
 
 app = Flask(__name__)
 app.debug = True
@@ -11,25 +12,14 @@ app.debug = True
 def index():
     return render_template('index.html')
 
-@app.route("/distributions")
-def distributions():
-	return render_template("distributions.html")
+@app.route("/distributions/<feature>")
+def distributions(feature):
+	return render_template("distributions.html", feature=feature)
 
-@app.route("/data/distributions")
-def data_distributions():
-	with open("japan_data09.json") as f:
-		data = json.load(f)
-
-	return flask.jsonify(results=[
-    {'Date': '01-03-2013', 'Views': 'a', 'Owner':'Alpha','Rating':'****'},
-    {'Date': '05-03-2013', 'Views': 'b', 'Owner':'Beta','Rating':'****'},
-    {'Date': '09-03-2013', 'Views': 'c', 'Owner':'Gamma','Rating':'**'},
-    {'Date': '13-03-2013', 'Views': 'd', 'Owner':'Beta','Rating':'****'},
-    {'Date': '01-04-2013', 'Views': 'a', 'Owner':'Theta','Rating':'****'},
-    {'Date': '05-04-2013', 'Views': 'b', 'Owner':'Beta','Rating':'***'},
-    {'Date': '09-04-2013', 'Views': 'c', 'Owner':'Theta','Rating':'**'},
-    {'Date': '13-04-2013', 'Views': 'd', 'Owner':'Beta','Rating':'*'},
-])
+@app.route("/data/distributions/<feature>")
+def data_distributions(feature):
+    data = database_helper.get_distributions(feature,num_bins=10)
+    return flask.jsonify(results=accuracies_data)
 
 @app.route("/data/geojson")
 def data_geojson():

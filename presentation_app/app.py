6956c26a9f22ec30e7c0a77e0de065e032d72d97
Anders <handiiandii@gmail.com>
2016-02-19 01:06:54 +0100

    Geo-data fetched from data and in a thread

--- presentation_app/app.py
+++ presentation_app/app.py
@@ -5,12 +5,32 @@ from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort
 import sys
 import os
+from threading import Thread
+from multiprocessing.pool import ThreadPool
 
 sys.path.append(os.path.join("..","tools"))
-import database_helper
+import DatabaseHelper
+import GeoData
 
 app = Flask(__name__)
 app.debug = True
+
+tools_path = "../tools/"
+database = DatabaseHelper.DatabaseHelper(tools_path)
+g = GeoData.GeoData(tools_path)
+
+#print("App: Henter Geo-data data...")
+try:
+    pool = ThreadPool(processes=1)
+    print("App: Henter Geo-data data...")
+    # tuple of args for foo, please note a "," at the end of the arguments
+    async_result = pool.apply_async(g.get_and_generate, ('Japan',))
+    #geo_data = Thread.(target=g.get_and_generate, args=("Japan"))
+except Exception:
+    print("Kunne ikke starte tråd")
+
+
+
 
 @app.route('/')
 def index():
@@ -22,14 +42,18 @@ def distributions(feature):
 
 @app.route("/data/distributions/<feature>")
 def data_distributions(feature):
-    data = database_helper.get_distributions(feature,num_bins=10)
+    data = database.get_distributions(feature,num_bins=10) #database_helper.get_distributions(feature,num_bins=10)
     return flask.jsonify(results=data)
 
 @app.route("/data/geojson")
 def data_geojson():
-	with open("geodata.geojson") as f:
-		data = json.load(f)
-	return flask.jsonify(data)
+    print("data_geojson aktiveret")
+    gjson_data = async_result.get()
+    print("Geo-data hentet!!")
+    #print(g.check_validity(gjson_data))
+	#with open("geodata.geojson") as f:
+    #data = json.load(gjson_data)   
+    return flask.jsonify(gjson_data)
 
 if __name__ == "__main__":
     app.run()

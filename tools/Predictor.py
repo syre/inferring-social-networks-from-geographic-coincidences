82569697d901eac0719d95ebf2aebcb697a54c6a
Søren Howe Gersager <syrelyre@gmail.com>
2016-03-13 13:54:55 +0100

    added spatial bin calculator for predictor

--- tools/Predictor.py
+++ tools/Predictor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import pybrain
 import DatabaseHelper
-
+import math
 database = DatabaseHelper.DatabaseHelper()
 
 class Predictor():
@@ -10,3 +10,16 @@ class Predictor():
 	
 	def generate_dataset(self):
 		pass
+
+	def calculate_spatial_bin(lng, lat, resolution_decimals=3):
+		GRID_MAX_LAT = 180 * pow(10,resolution_decimals)
+
+		lat += 90
+		lng += 180
+
+		lat = math.trunc(lat*pow(10,resolution_decimals))
+		lng = math.trunc(lng*pow(10,resolution_decimals))
+
+		return (GRID_MAX_LAT * lat) + lng
+
+

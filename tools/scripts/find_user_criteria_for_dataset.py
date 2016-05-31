fbdc0432ed5da639291c1f892652cc335bb47bfc
Søren Howe Gersager <syrelyre@gmail.com>
2016-05-31 16:20:58 +0200

    added criteria

---
+++ tools/scripts/find_user_criteria_for_dataset.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+"""
+Finds users with locations for all days of the three months
+"""
+import sys
+import os
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from collections import defaultdict
+import numpy as np
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from DatabaseHelper import DatabaseHelper
+
+db = DatabaseHelper()
+plt.style.use("ggplot")
+
+first_period_datetime_min = "2015-09-01 00:00:00+00:00"
+first_period_time_bin_min = db.calculate_time_bins(first_period_datetime_min)[0]
+first_period_datetime_max = "2015-09-30 23:59:59+00:00"
+first_period_time_bin_max = db.calculate_time_bins(first_period_datetime_max)[0]
+second_period_datetime_min = "2015-10-01 00:00:00+00:00"
+second_period_time_bin_min = db.calculate_time_bins(second_period_datetime_min)[0]
+second_period_datetime_max = "2015-10-31 23:59:59+00:00"
+second_period_time_bin_max = db.calculate_time_bins(second_period_datetime_max)[0]
+third_period_datetime_min = "2015-11-01 00:00:00+00:00"
+third_period_time_bin_min = db.calculate_time_bins(third_period_datetime_min)[0]
+third_period_datetime_max = "2015-11-30 23:59:59+00:00"
+third_period_time_bin_max = db.calculate_time_bins(third_period_datetime_max)[0]
+
+
+def compute_cdf_in_three_periods(country):
+    locations = db.get_locations_by_country_only(country)
+    users = set([l[0] for l in locations])
+    user_timebins_dict = defaultdict(lambda: defaultdict(set))
+    for l in locations:
+        timebins = l[4]
+        for timebin in timebins:
+            if timebin > first_period_time_bin_min and timebin < first_period_time_bin_max:
+                user_timebins_dict[l[0]]["first_period"].add(timebin)
+            elif timebin > second_period_time_bin_min and timebin < second_period_time_bin_max:
+                user_timebins_dict[l[0]]["second_period"].add(timebin)
+            elif timebin > third_period_time_bin_min and timebin < third_period_time_bin_max:
+                user_timebins_dict[l[0]]["third_period"].add(timebin)
+
+    for user, blabla in user_timebins_dict.items():
+        blabla["first_period"] = len(blabla["first_period"])/(first_period_time_bin_max-first_period_time_bin_min)
+        blabla["second_period"] = len(blabla["second_period"])/(second_period_time_bin_max-second_period_time_bin_min)
+        blabla["third_period"] = len(blabla["third_period"])/(third_period_time_bin_max-third_period_time_bin_min)
+
+    def cdf(x): return len([(user, p) for user, p in user_timebins_dict.items() if p["first_period"] > x and p["second_period"] > x and p["third_period"] > x])
+    return [cdf(x) for x in np.arange(0, 1, 0.01)]
+
+
+sweden, = plt.plot(np.arange(0, 1, 0.01), compute_cdf_in_three_periods("Sweden"), linewidth=6.0, label="Sweden", color='#990033')
+japan, = plt.plot(np.arange(0, 1, 0.01), compute_cdf_in_three_periods("Japan"), linewidth=6.0, label="Japan", color='#333399', marker="+")
+
+first_period_datetime_min = "2015-09-01 00:00:00+00:00"
+first_period_time_bin_min = db.calculate_time_bins(first_period_datetime_min)[0]
+first_period_datetime_max = "2015-09-09 23:59:59+00:00"
+first_period_time_bin_max = db.calculate_time_bins(first_period_datetime_max)[0]
+second_period_datetime_min = "2015-09-10 00:00:00+00:00"
+second_period_time_bin_min = db.calculate_time_bins(second_period_datetime_min)[0]
+second_period_datetime_max = "2015-09-19 23:59:59+00:00"
+second_period_time_bin_max = db.calculate_time_bins(second_period_datetime_max)[0]
+third_period_datetime_min = "2015-09-20 00:00:00+00:00"
+third_period_time_bin_min = db.calculate_time_bins(third_period_datetime_min)[0]
+third_period_datetime_max = "2015-09-30 23:59:59+00:00"
+third_period_time_bin_max = db.calculate_time_bins(third_period_datetime_max)[0]
+
+sweden_sept, = plt.plot(np.arange(0, 1, 0.01), compute_cdf_in_three_periods("Sweden"), linewidth=6.0, label="Sweden - september", color='#990033')
+japan_sept, = plt.plot(np.arange(0, 1, 0.01), compute_cdf_in_three_periods("Japan"), linewidth=6.0, label="Japan - september", color='#333399', marker="+")
+
+plt.legend(handles=[sweden, japan, sweden_sept, japan_sept], prop={'size': 40})
+plt.ylabel("Number of users")
+plt.xlabel("Percentage of total timebins")
+plt.title("CDF for unique timebins in all three months")
+plt.tick_params(labelsize=20)
+[item.set_fontsize(45) for item in [plt.gca().yaxis.label, plt.gca().xaxis.label]]
+plt.gca().title.set_fontsize(48)
+[item.set_fontsize(33) for item in plt.gca().get_xticklabels() + plt.gca().get_yticklabels()]
+plt.show()
+
+user_locations_dict = {}
+
+
+def get_users_with_unique_days(number):
+    """
+    Find test and train users with at least 'number' unique days in
+    both test or train months
+    """
+    train_users = []
+    test_users = []
+    for u, location_dates in user_locations_dict.items():
+        sept_locations = [x for x in location_dates if x[1] == 9]
+        oct_locations = [x for x in location_dates if x[1] == 10]
+        nov_locations = [x for x in location_dates if x[1] == 11]
+        if len(set(sept_locations)) >= number and len(set(oct_locations)) >= number:
+            train_users.append(u)
+        if len(set(oct_locations)) >= number and len(set(nov_locations)) >= number:
+            test_users.append(u)
+    return train_users, test_users
+
+
+def generate_plot(min=0, max=25):
+    train_values = []
+    test_values = []
+    for x in range(min, max):
+        train_users, test_users = get_users_with_unique_days(x)
+        train_values.append(len(train_users))
+        test_values.append(len(test_users))
+    plt.title("Number of users with unique days with location updates (Sweden)")
+    plt.xlabel("number of unique days with updates in both months")
+    plt.ylabel("number of users")
+    plt.plot(range(min, max), train_values, label="train users (sept, oct)")
+    plt.plot(range(min, max), test_values, label="test users (oct, nov)")
+    plt.legend()
+    plt.show()
+
+#if __name__ == '__main__':
+    #print(get_users_with_unique_days(25))

73ca5ab53578d7cc715203b2556c5e0b117752fc
Søren Howe Gersager <syrelyre@gmail.com>
2016-06-16 17:31:32 +0200

    random forest hyperparameter

--- tools/scripts/model_performance.py
+++ tools/scripts/model_performance.py
@@ -114,12 +114,9 @@ def undersample(X_train, y_train):
 def plot_performance(all_data):
     param_grid = {"max_depth": [3, None],
                   "max_features": sp_randint(1, 10),
-                  "min_samples_split":  sp_randint(1, 10),#[1, 3, 10],
+                  "min_samples_split":  sp_randint(1, 10),
                   "min_samples_leaf": sp_randint(1, 11),
-                  "bootstrap": [True, False],
-                  "criterion": ["gini", "entropy"],
-                  "random_state": [0],
-                  "class_weight": ["balanced", None]
+                  "criterion": ["gini", "entropy"]
                   }
     for i, pair in enumerate(all_data, start=1):
         ax = plt.subplot(2, 2, i)
@@ -129,12 +126,21 @@ def plot_performance(all_data):
         mean_tpr_rf = 0.0
         mean_fpr_rf = np.linspace(0, 1, 100)
         pair_name = ""
-        for data in pair:
+        precision_score_lr = [0,0]
+        precision_score_rf = [0,0]
+        recall_score_lr = [0,0]
+        recall_score_rf = [0,0]
+        importances = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+        for j, data in enumerate(pair):
             pair_name = data[6]
-           
+
             lr = sklearn.linear_model.LogisticRegression(random_state=0, class_weight="balanced")
             lr.fit(data[2].reshape(-1, 1), data[1])
             y_pred = lr.predict_proba(data[5].reshape(-1, 1))[:, 1]
+            precision_score_lr[0] += sklearn.metrics.precision_score(data[4], lr.predict(data[5].reshape(-1, 1)), pos_label=0)
+            recall_score_lr[0] += sklearn.metrics.recall_score(data[4], lr.predict(data[5].reshape(-1, 1)), pos_label=0)
+            precision_score_lr[1] += sklearn.metrics.precision_score(data[4], lr.predict(data[5].reshape(-1, 1)), pos_label=1)
+            recall_score_lr[1] += sklearn.metrics.recall_score(data[4], lr.predict(data[5].reshape(-1, 1)), pos_label=1)
             # logistic regression ROC
             false_positive_rate, true_positive_rate, _ = roc_curve(data[4], y_pred)
             mean_tpr_lr += interp(mean_fpr_lr, false_positive_rate, true_positive_rate)
@@ -147,11 +153,18 @@ def plot_performance(all_data):
             grid_search.fit(undersample_data[0], undersample_data[1])
             print(grid_search.best_params_)
             y_pred = grid_search.predict_proba(data[3])[:, 1]
+            precision_score_rf[0] += sklearn.metrics.precision_score(data[4], grid_search.predict(data[3]), pos_label=0)
+            recall_score_rf[0] += sklearn.metrics.recall_score(data[4], grid_search.predict(data[3]), pos_label=0)
+            precision_score_rf[1] += sklearn.metrics.precision_score(data[4], grid_search.predict(data[3]), pos_label=1)
+            recall_score_rf[1] += sklearn.metrics.recall_score(data[4], grid_search.predict(data[3]), pos_label=1)
             # forest ROC
             false_positive_rate, true_positive_rate, _ = roc_curve(
                 data[4], y_pred)
             mean_tpr_rf += interp(mean_fpr_rf, false_positive_rate, true_positive_rate)
-
+            if i == 3:
+               importances = np.add(grid_search.best_estimator_.feature_importances_, importances)
+        if i == 3 and j == 1:
+            print(importances/2)
         mean_tpr_lr /= 2
         mean_tpr_rf /= 2
 
@@ -161,7 +174,8 @@ def plot_performance(all_data):
         mean_auc_lr = auc(mean_fpr_lr, mean_tpr_lr)
 
         mean_auc_rf = auc(mean_fpr_rf, mean_tpr_rf)
-
+        print("Precision,Recall score (LR) for negative: {},{} and positive: {},{}".format(precision_score_lr[0]/2, recall_score_lr[0]/2, precision_score_lr[1]/2, recall_score_lr[1]/2))
+        print("Precision,Recall score (RF) for negative: {},{} and positive: {},{}".format(precision_score_rf[0]/2, recall_score_rf[0]/2, precision_score_rf[1]/2, recall_score_rf[1]/2))
         # Compute ROC curve and ROC area for each class
         plt.title('Receiver Operating Characteristic for ' + pair_name)
         plt.plot(mean_fpr_lr, mean_tpr_lr, 'r',

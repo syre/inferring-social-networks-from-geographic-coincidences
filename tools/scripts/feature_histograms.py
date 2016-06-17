66051c271a655c0ca69344e7f05e06bbf3ac6122
Anders <handiiandii@gmail.com>
2016-06-17 15:41:29 +0200

    feature boxplots updated

--- tools/scripts/feature_histograms.py
+++ tools/scripts/feature_histograms.py
@@ -22,25 +22,30 @@ test_features = ["has_two_unique_coocs", "arr_leav", "diversity", "unique_coocs"
 prod_features = ["num_coocs", "num_unique_coocs", "diversity", "weighted_frequency",
  "sum_weekends", "sum_evenings", "coocs_w", "mutual_cooccurrences", "specificity"]
 
-f = plt.figure(figsize=(10, 10))
-#y_train = y_train[X_train[:,0] < 100]
-#X_train = X_train[X_train[:,0] < 100]
+
 meets = X_train[np.where(y_train == 1)]
 nonmeets = X_train[np.where(y_train == 0)]
-#sns.set(font_scale=2.5)
-#sns.distplot(meets[:,5], hist=True, norm_hist=True, axlabel=features[5], color="blue", kde=False, hist_kws={"alpha":0.5})
+figure = 1
+f = plt.figure(figure, figsize=(10, 10))
+plot_index = 1
 for index, feature in enumerate(test_features):
-    ax = f.add_subplot(8,2,index+1)
+    ax = f.add_subplot(2, 2, plot_index)
     ax.set_title(feature)
-    #sns.distplot(meets[:,index], ax=ax, hist=True, norm_hist=True, axlabel=feature, color="blue", kde=False, hist_kws={"alpha":0.5})
-    #sns.distplot(nonmeets[:,index], ax=ax, hist=True, norm_hist=True, axlabel=feature, color="red", kde=False, hist_kws={"alpha":0.5})
-    all_events = np.hstack((meets[:,index], nonmeets[:,index]))
-    types = np.hstack((["did meet" for x in range(len(meets[:,index]))], ["did not meet" for x in range(len(nonmeets[:,index]))]))
+    all_events = np.hstack((meets[:, index], nonmeets[:, index]))
+    types = np.hstack((["did meet" for x in range(len(meets[:, index]))], ["did not meet" for x in range(len(nonmeets[:,index]))]))
     sns.boxplot(x=types, y=all_events, ax=ax)
-    
-    #[item.set_fontsize(35) for item in [ax.yaxis.label, ax.xaxis.label]]
-    #ax.title.set_fontsize(40)
-    #[item.set_fontsize(28) for item in ax.get_xticklabels() + ax.get_yticklabels()]
+    [item.set_fontsize(35) for item in [ax.yaxis.label, ax.xaxis.label]]
+    ax.title.set_fontsize(40)
+    [item.set_fontsize(28) for item in ax.get_xticklabels() + ax.get_yticklabels()]
+    plot_index += 1
+    if plot_index == 5:
+        plt.show()
+        figure += 1
+        f = plt.figure(figure, figsize=(10, 10))
+        plot_index = 1
+[item.set_fontsize(35) for item in [ax.yaxis.label, ax.xaxis.label]]
+ax.title.set_fontsize(40)
+[item.set_fontsize(28) for item in ax.get_xticklabels() + ax.get_yticklabels()]
 
 f.tight_layout()
 plt.show()

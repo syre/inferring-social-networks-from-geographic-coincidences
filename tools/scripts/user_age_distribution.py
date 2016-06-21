59e183239d13d6f46570104c23e70076155e6d62
Søren Howe Gersager <syrelyre@gmail.com>
2016-06-21 17:26:59 +0200

    added demographics data

--- tools/scripts/user_age_distribution.py
+++ tools/scripts/user_age_distribution.py
@@ -6,13 +6,13 @@ import FileLoader
 from collections import Counter
 import seaborn as sns
 import DatabaseHelper
+import matplotlib.pyplot as plt
 
 d = DatabaseHelper.DatabaseHelper()
 distinct_users = d.get_distinct_feature("useruuid", "location")
 
-
-sns.set(color_codes=True)
-sns.set(style="white", palette="muted")
+colors = ['#3498db', '#e74c3c']
+sns.set_palette(colors)
 file_loader = FileLoader.FileLoader()
 user_info_dict = file_loader.generate_demographics_from_csv()
 
@@ -25,8 +25,11 @@ birthdates = [value["birthdate"] for value in user_info_dict.values()]
 #print([u for u in user_info_dict.values() if u["age"] < 16])
 #print(len([u for u in user_info_dict.values() if u["birthdate"].endswith("-01")]), len(user_info_dict.values()))
 print(Counter(birthdates).most_common(20))
-ax = sns.distplot(ages)
-ax.set(xlabel="age")
+ax = sns.distplot(ages, kde=False)
+ax.set(xlabel="Age", title="Distribution of user ages", ylabel="Frequency")
 ax.set_xlim(0)
+plt.gca().title.set_fontsize(20)
+[item.set_fontsize(14) for item in plt.gca().get_xticklabels() + plt.gca().get_yticklabels()]
+[item.set_fontsize(14) for item in [plt.gca().yaxis.label, plt.gca().xaxis.label]]
 #sns.boxplot(y=ages)
 sns.plt.show()

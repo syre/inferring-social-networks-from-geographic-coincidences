f8aa24d4b2e28981ee28afaf87a775ea687b87bb
Søren Howe Gersager <syrelyre@gmail.com>
2016-07-01 15:30:58 +0200

    graph

--- tools/scripts/graph_cdf.py
+++ tools/scripts/graph_cdf.py
@@ -3,16 +3,19 @@ import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import networkx as nx
-import seaborn
+import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 directory = os.path.join("..", "data", "sweden.graphml")
 
 sweden_graph = nx.read_graphml(directory)
 degrees = list(sweden_graph.degree().values())
-print(degrees)
-seaborn.distplot(degrees, kde=False)
-plt.xlim(0, 250)
-plt.xlabel("Value")
-plt.ylabel("Count")
+sorted_data = np.sort(degrees)
+yvals = np.arange(len(sorted_data))/float(len(sorted_data))
+sns.set_style("whitegrid")
+plt.title("CDF of degree")
+plt.plot(sorted_data, yvals)
+plt.xlabel("Degree")
+plt.ylabel("Probability")
 plt.show()
